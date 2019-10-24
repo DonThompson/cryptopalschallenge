7b14da4a2848cfb66d330a65cb57eb7f1e1936f5
Don Thompson <thompsondn@gmail.com>
2019-10-24 16:38:45 -0400

    Functioning challenge 01

--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
-﻿using System;
-
-namespace cryptopalschallenge
+﻿namespace cryptopalschallenge
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //TODO - arg check
+            //TODO - split by challenges
+
+
+            Challenge01 c = new Challenge01();
+            c.DoChallenge01(args[0]);
+
+
         }
+
     }
 }
