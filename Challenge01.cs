fc73eb81b330db68bee574978987560712e3a02c
Don Thompson <DonThompson@users.noreply.github.com>
2019-10-24 16:39:16 -0400

    Functioning challenge 01 (#1)

---
+++ Challenge01.cs
@@ -0,0 +1,23 @@
+﻿using cryptopalschallenge.Tools;
+using System;
+
+namespace cryptopalschallenge
+{
+    public class Challenge01
+    {
+        /// <summary>
+        /// Given:  49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+        /// 
+        /// Output:  SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+        /// </summary>
+        /// <param name="hexString"></param>
+        public void DoChallenge01(string hexString)
+        {
+            byte[] byteArray = HexStringToByteArrayConverter.Convert(hexString);
+
+            string output = Convert.ToBase64String(byteArray);
+
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ Tools/HexStringToByteArrayConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace cryptopalschallenge.Tools
+{
+    public class HexStringToByteArrayConverter
+    {
+        /// <summary>
+        /// Given a hex string "A1B2C4F8" returns a byte array containing "A1", "B2", C4", F8"
+        /// </summary>
+        /// <param name="hexString">Must be an even number of characters or an exception will be thrown</param>
+        /// <returns>The byte array will be of length hexString/2</returns>
+        public static byte[] Convert(string hexString)
+        {
+            //Convert hex string to a byte array - each 2 characters are a hex byte
+            if (hexString.Length % 2 == 1)
+            {
+                throw new Exception("Invalid hex string input specified.  Please ensure it has a even number of digits.");
+            }
+
+            byte[] byteArray = new byte[hexString.Length / 2];
+            for (int i = 0; i < hexString.Length / 2; i++)
+            {
+                string byteValue = hexString.Substring(i * 2, 2);
+                byteArray[i] = byte.Parse(byteValue, NumberStyles.HexNumber);
+            }
+
+            return byteArray;
+        }
+    }
+}
