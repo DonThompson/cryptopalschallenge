5dd30db1670bf6e18eaa89e027dc551836e8d79e
Don Thompson <DonThompson@users.noreply.github.com>
2019-10-24 20:14:36 -0400

    Challenge02 (#2)
    * Completed challenge.  Cleanup needed
    
    * cleanup & refactoring

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using CommandLine;
-using CommandLine.Text;
 using System;
 
 namespace cryptopalschallenge
@@ -20,16 +19,8 @@ namespace cryptopalschallenge
 
         static void Main(string[] args)
         {
-            //TODO - arg check
-            //TODO - split by challenges
-
             CommandLine.Parser.Default.ParseArguments<Options>(args)
                 .WithParsed<Options>(opts => RunOptionsAndReturnExitCode(opts));
-
-
-            
-
-
         }
 
         static void RunOptionsAndReturnExitCode(Options opts)
@@ -37,8 +28,13 @@ namespace cryptopalschallenge
             switch (opts.Challenge) { 
                 case 1:
                     //-c 01 -x 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
-                    Challenge01 c = new Challenge01();
-                    c.DoChallenge01(opts.Value1);
+                    Challenge01 c1 = new Challenge01();
+                    Console.WriteLine(c1.DoChallenge01(opts.Value1));
+                    break;
+                case 2:
+                    //-c 02 -x 1c0111001f010100061a024b53535009181c -y 686974207468652062756c6c277320657965
+                    Challenge02 c2 = new Challenge02();
+                    Console.WriteLine(c2.DoChallenge02(opts.Value1, opts.Value2));
                     break;
                 default:
                     Console.WriteLine("Invalid options specified.");
