41418220a11b379bf305660609f4add35e6482c4
Don Thompson <thompsondn@gmail.com>
2019-10-24 21:35:18 -0400

    Refactor the c03 code so i can better re-use it for c04

--- cryptopalschallenge/Challenge03.cs
+++ cryptopalschallenge/Challenge03.cs
@@ -1,5 +1,4 @@
-﻿using System.Text;
-using cryptopalschallenge.Tools;
+﻿using cryptopalschallenge.Tools;
 
 namespace cryptopalschallenge
 {
@@ -8,47 +7,16 @@ namespace cryptopalschallenge
     /// </summary>
     public class Challenge03
     {
-        private double highScore = 0.0;
-        private byte bestKey = 0;
-
         /// <summary>
         /// Given a string encoded by XOR'ing only a single character, figure out the character
         /// </summary>
         /// <param name="input"></param>
         public string DoChallenge03(string input)
         {
-            byte[] data = HexStringToByteArrayConverter.Convert(input);
+            SingleCharXORDecoder decoder = new SingleCharXORDecoder();
+            decoder.TestAllKeys(input);
 
-            //Work over the whole range of the byte (0-255)
-            for(int key = 0; key < 256; key++)
-            {
-                //We need a byte array of the same length with the character to test
-                byte[] testKey = new byte[data.Length];
-                for(int i = 0; i < data.Length; i++)
-                {
-                    testKey[i] = (byte)key;
-                }
-
-                //Now XOR it
-                byte[] result = XOR.ExclusiveOR(data, testKey);
-
-                //See if it rates as a valid string?
-                string readable = Encoding.UTF8.GetString(result);
-                CheckHighScore(readable, (byte)key);
-            }
-
-            return ((char)bestKey).ToString();
-        }
-
-        private void CheckHighScore(string readable, byte key)
-        {
-            //Eval for high score
-            double score = LetterFrequencyScorer.ScoreStringBasic(readable);
-            if (score > highScore)
-            {
-                highScore = score;
-                bestKey = key;
-            }
+            return decoder.BestResults.BestKey;
         }
     }
 }
