fcc9c83bb23a6dfb586b00ecc1ae6a4869ba72e2
Don Thompson <thompsondn@gmail.com>
2019-11-04 16:23:18 -0500

    class to edit bit distance between 2 strings

---
+++ cryptopalschallenge.Tests/EditDistanceComputerTests.cs
@@ -0,0 +1,23 @@
+﻿using cryptopalschallenge.Tools;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cryptopalschallenge.Tests
+{
+    [TestClass]
+    public class EditDistanceComputerTests
+    {
+        [TestMethod]
+        public void ExpectedTest()
+        {
+            string s1 = "this is a test";
+            string s2 = "wokka wokka!!!";
+
+            EditDistanceComputer c = new EditDistanceComputer();
+            int distance = c.ComputeBitDistanceBetween(s1, s2);
+            Assert.AreEqual(37, distance);
+        }
+    }
+}
---
+++ cryptopalschallenge/Tools/EditDistanceComputer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace cryptopalschallenge.Tools
+{
+    public class EditDistanceComputer
+    {
+        /// <summary>
+        /// Converts given UTF8 strings to bits and compares the bit distance between them.
+        /// </summary>
+        /// <param name="s1">A plain text UTF8 string</param>
+        /// <param name="s2">A plain text UTF8 string</param>
+        /// <returns>The number of differing bits</returns>
+        public int ComputeBitDistanceBetween(string s1, string s2)
+        {
+            byte[] b1 = Encoding.UTF8.GetBytes(s1);
+            byte[] b2 = Encoding.UTF8.GetBytes(s2);
+
+            if(b1.Length != b2.Length)
+            {
+                throw new Exception("Cannot compute distance on strings of differing length.");
+            }
+
+
+            int distance = 0;
+            //Iterate through each byte
+            for(int i = 0; i < b1.Length; i++)
+            {
+                //Bitwise Calculation from wikipedia... https://en.wikipedia.org/wiki/Hamming_distance
+                //Iterate through each bit in the byte.  If they don't match, add to our distance.  Could also do this with some bitwise shifting operators.
+                for (uint val = (uint)(b1[i] ^ b2[i]); val > 0; val /= 2)
+                {
+                    uint test = val & 1;
+
+                    if (test != 0)
+                        distance++;
+                }
+            }
+
+            return distance;
+        }
+    }
+}
