d376ab5e8428c2ea684a9669d398f9111e2fa375
Don Thompson <thompsondn@gmail.com>
2019-11-04 15:46:21 -0500

    Initial completion, needs refactored.

---
+++ cryptopalschallenge/Challenge05.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cryptopalschallenge
+{
+    /// <summary>
+    /// https://cryptopals.com/sets/1/challenges/5
+    /// </summary>
+    public class Challenge05
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string RepeatingKeyXOR(string input, string key)
+        {
+            byte[] inputArray = Encoding.UTF8.GetBytes(input);
+            byte[] keyArray = Encoding.UTF8.GetBytes(key);
+
+            byte[] result = new byte[inputArray.Length];
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                    //XOR each byte of input by a rotation through the key
+                    result[i] = (byte)(inputArray[i] ^ keyArray[i%keyArray.Length]);
+            }
+
+            return BitConverter.ToString(result).Replace("-", "");
+
+        }
+    }
+}
